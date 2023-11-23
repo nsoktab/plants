9b7c92afe9f3430f111c642fe3e9a5a20c840f0b
nsoktab <nataliasokolowskaa@gmail.com>
2023-11-23 12:31:44 +0100

    changes

---
+++ src/components/Student.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function Student() {
+  const [user, setUser] = useState;
+  useEffec;
+  return (
+    <div>
+      <h1>Student</h1>
+    </div>
+  );
+}
